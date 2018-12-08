82a4a2c8826b2825d5e759697738bb20c3475f9d
Shivam Aggarwal <shivam.aggarwal1597@gmail.com>
2018-12-08 17:55:33 +0530

    Third Commit

--- app/src/main/java/com/example/shivam/attendanceappminor/AdminDashboardActivity.java
+++ app/src/main/java/com/example/shivam/attendanceappminor/AdminDashboardActivity.java
@@ -10,7 +10,7 @@ public class AdminDashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
 
-        //TODO : Add Students using excel sheet
+        //TODO : Add Students using excel sheet for login validation purpose
         // Data to be Added
         //Student_name
         //Student_id(enrollment number)
@@ -20,6 +20,20 @@ public class AdminDashboardActivity extends AppCompatActivity {
         //batch_start_year
         //batch_end_year
         //stream
+        //mobile_number
+        //gender
+
+        //ToDO : Add Faculty using excel sheet for login validation purpose
+        //Data to be added
+        //faculty_name
+        //faculty_id
+        //dob
+        //password
+        //gender
+        //mobile_number
+
+        //This needs to be done only once by the administrator to register all students and faculties
+        //so that they can be accessed from their respective dashboards.
 
     }
 }
